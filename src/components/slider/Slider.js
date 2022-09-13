8cc37ab9f8fac6903701dd425f266287daf50481
Monnus <maun2velsket@gmail.com>
2022-09-13 15:35:16 +0200

    made fab

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import ViewSlider from 'react-native-view-slider';
+
+const SliderImage=()=>{
+  return (
+    <View>
+      <Text></Text>
+     </View>
+  );
+}
+
+export default  SliderImage
