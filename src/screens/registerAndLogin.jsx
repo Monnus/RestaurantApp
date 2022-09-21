edff9316a9b42a330893646d7e32b8e3f4bfdaa6
Monnus <maun2velsket@gmail.com>
2022-09-21 16:12:30 +0200

    created loginScreen

--- src/screens/registerAndLogin.jsx
+++ src/screens/registerAndLogin.jsx
@@ -5,39 +5,70 @@ import { Button } from 'react-native-paper';
 import {getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword} from "firebase/auth";
 import app from '../../firebaseConfig';
 
+
+// ====================== uid "BEaSox2mhiY1tFj7GLAiJspS9bi1"============================
+
 const RegisterAndLogin=()=> {
     const [refOrLog,setRefORLog]=useState(1)
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("");
     const auth=getAuth(app)
+// ============================handle Submit===========================
+    const handleSubmit=(refOrLog)=>{
+        console.log(refOrLog,"hello",email,password);
+    if(refOrLog==1){
+    createUserWithEmailAndPassword(auth,email,password).then((res=>{
+    console.log(res,"account created seccssfully");
+    setemail("");
+    setpassword("");
+    setRefORLog(2);
+    })).catch((err)=>console.log(err.message))
+    }else if(refOrLog==2){
+    signInWithEmailAndPassword(auth,email,password).then((res)=>{
+    console.log(res,"sign in with email");
+    const userObj= Object.assign(res);
+    console.log(userObj);
+    }).catch((err)=>console.log(err.message));
+    }
+    };
+
     const registerScreen=()=>{
-        return( 
- 
-    <View>
+        return(
+            <> 
+ <View style={{height:"400px",width:"100%",backgroundColor:"gray"}}> 
+
+Regestration page
+ </View>
+    <View style={{margin:"50px "}}>
     <TextInput placeholder='Email' style={styles.inputBtn} onChange={(e)=>setemail(e.target.value)} value={email}/>
-    <TextInput placeholder='Password' style={styles.inputBtn}  onChange={(e)=>setemail(e.target.value)} value={password}/>
-    <Button onPress={(e)=>console.log("home")}>Submite</Button>
-    <Text onPress={()=>console.log("hello")} >Go to sign in page</Text>
+    <TextInput placeholder='Password' style={styles.inputBtn}  onChange={(e)=>setpassword(e.target.value)} value={password}/>
+    <Button onPress={()=>handleSubmit(refOrLog)}>Submite</Button>
+    <Text onPress={()=>setRefORLog(2)}>Go to sign in page</Text>
     </View>
-  
+</>
         )
     }
 
-    const handleSubmit=(refOrLog)=>{
-if(refOrLog==1){
-createUserWithEmailAndPassword(auth,email,password).then((res=>{
-    console.log(res,"account created seccssfully");
-})).catch((err)=>console.log(err))
-}else if(refOrLog==2){
-    signInWithEmailAndPassword(auth,email,password).then((res)=>{
-    console.log(res,"sign in with email");
-    const userObj=new Object.assign(res)
-    }).catch((err)=>console.log(err);)
+const loginInScreen=()=>{
+    return (
+<>
+<View style={{height:"400px",width:"100%",backgroundColor:"skyblue"}}> 
+
+login Page
+ </View>
+        <View style={{margin:"50px"}}>
+        <TextInput placeholder='Email' style={styles.inputBtn} onChange={(e)=>setemail(e.target.value)} value={email}/>
+        <TextInput placeholder='Password' style={styles.inputBtn}  onChange={(e)=>setpassword(e.target.value)} value={password}/>
+        <Button onPress={()=>handleSubmit(refOrLog)}>Submite</Button>
+        <Text onPress={()=>setRefORLog(1)}>Go to registration  page</Text>
+        </View>
+</>
+    )
 }
-    };
+
             return (
             <SafeAreaView style={styles.container}>
-       {registerScreen()}
+       {refOrLog==1?registerScreen():loginInScreen()}
     
                 </SafeAreaView>
                  );
@@ -45,14 +76,22 @@ createUserWithEmailAndPassword(auth,email,password).then((res=>{
 
 const styles = StyleSheet.create({
     container:{
+        display:"flex",
+   
             flex:1,
             width:"100%",
+          
     },
+    
     inputBtn:{
         width:300,
         backgroundColor:"white",
         height:50,
-        marginBottom:10
+        marginBottom:10,
+        paddingLeft:20,
+        fontSize:20,
+        outline:"",
+        color:"gray"
     }
     
 })
