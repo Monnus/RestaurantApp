dc3954c51bfa2ebfa9e8af56c56f6a0294cbb171
Monnus <maun2velsket@gmail.com>
2022-09-23 15:09:22 +0200

    created profilescreen

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,49 +1,134 @@
-import React,{useState}from 'react';
-import { View, Text , StyleSheet,Image,TextInput , SafeAreaView} from 'react-native';
-import{getFirestore,collection,addDoc} from "firebase/firestore"
+import React,{useState,useEffect}from 'react';
+import { View, Text , StyleSheet,Image,TextInput ,Platform,SafeAreaView} from 'react-native';
+import{getFirestore,collection,addDoc,getDocs,doc, updateDoc} from "firebase/firestore";
+import {getAuth, onAuthStateChanged,updateProfile} from "firebase/auth";
+import * as ImagePicker from "expo-image-picker";
 import app from '../../firebaseConfig';
 import Headerv2 from '../components/headerv2/Headerv2';
 import Menu from '../components/manu/menue';
 import { Button } from 'react-native-paper';
+import { async } from '@firebase/util';
+import Elevations from 'react-native-elevation';
+
 export default function Profile() {
+const defaultImage=require("../Image/emptyIcon.png");    
+    const [image,setImage]=useState(null)
+    const [userId,setUserID]=useState("")
     const [showMenu,setShowMenu]=useState(false);
+    const [preferedName,setPreferedName]=useState("");
+    const [addNumber,setaddNumber]=useState("");
+    const [otherEmail,setOtherEmail]=useState("");
+ const auth=getAuth(app);
+
+
+    const db= getFirestore(app);
+  
+useEffect(()=>{
+     onAuthStateChanged(auth,(user)=>{
+        if(user){
+           console.log(user,"user Signed In");
+     
+        }else{
+            console.log("no user found");
+        }
+     })
+},[]);
+
+const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  const user=auth.currentUser;
+
+    const handleUpdate=async(preferedName,addNumber,otherEmail)=>{
+        try {
+            const docRe2=doc(db,"users","0VIqBWynmNHZXeq0Exr9")
+            const user=auth.currentUser;
+            updateProfile(auth.currentUser,{
+                displayName:preferedName,photoURL:image
+            })
+            const docRef= updateDoc(docRe2,{
+            signInEmail:user.email,
+            uid:user.uid,
+            displayName:user.displayName,
+            mobileNumber:addNumber,
+            ExtNumber:otherEmail,
+            profileImage:user.photoURL,
+            })
+            const querySnapshot = getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data()}`);
+        });
+
+            console.log("document written with ID:",docRef.id);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+  
 
     return (
         <SafeAreaView style={{flex:1,justifyContent:"center"}}>
         <Headerv2 showMenu={showMenu} setShowMenu={setShowMenu}/>
-            <View style={styles.container}>
-<Text>Prefered Name: 
- <TextInput placeholder='User' style={{height:30, borderLeftWidth:"2px",borderRightWidth:"2px",borderTopWidth:"2px",borderBottomWidth:"2px"}}/>           
+            <View style={styles.container} >
+                {image?
+            <Image source={{uri:image}} style={{width:"150px",height:"100px",marginBottom:"5px",...Elevations[4]}}/>
+            : <Image source={{uri:require("../Image/emptyIcon.png")}} style={{width:"150px",height:"100px",marginBottom:"5px",...Elevations[4]}}/>
+        }
+            <Button color="#2D3E48" mode="contained" style={{width:100,height:35,...Elevations[2]}} onPress={pickImage}><Text>Upload</Text></Button>
+    <Text style={styles.txtInput}>Prefered Name: 
+        <TextInput placeholder='User'
+            value={preferedName} onChange={(e)=>setPreferedName(e.target.value)}
+            style={{paddingLeft:"10px",height:30, borderLeftWidth:"2px",borderRightWidth:"2px",borderTopWidth:"2px",borderBottomWidth:"2px"}}/> 
+            </Text>
+       <Text style={styles.txtInput}>Phone Number: 
+        <TextInput placeholder='Number' value={addNumber}
+                onChange={(e)=>setaddNumber(e.target.value)} style={{paddingLeft:"10px",height:30, borderLeftWidth:"2px",borderRightWidth:"2px",borderTopWidth:"2px",borderBottomWidth:"2px"}}/>           
 
 </Text>
-       
-<Text>Prefered Name: 
- <TextInput placeholder='User' style={{height:30, borderLeftWidth:"2px",borderRightWidth:"2px",borderTopWidth:"2px",borderBottomWidth:"2px"}}/>           
-
+<Text style={styles.txtInput}>Other Email: 
+ <TextInput placeholder='input' value={otherEmail}
+ onChange={(e)=>setOtherEmail(e.target.value)} style={{paddingLeft:"10px",height:30, borderLeftWidth:"2px",borderRightWidth:"2px",borderTopWidth:"2px",borderBottomWidth:"2px"}}/>           
 </Text>
-            </View>
- {showMenu?<Menu setShowMenu={setShowMenu} showMenu={showMenu} navigation={navigation }/>:<></>}  
-    
-<Button mode="contained" color="#2D3E48" style={{marginLeft:"60px",marginBottom:"30px",width:"200px",height:"40px"}}>
+    </View>
+ {showMenu?<Menu setShowMenu={setShowMenu} showMenu={showMenu} navigation={navigation }/>:<></>}      
+<Button mode="contained" color="#2D3E48" 
+onPress={()=>handleUpdate(preferedName,addNumber,otherEmail)}
+ style={{marginLeft:"60px",marginBottom:"30px",width:"200px",height:"40px",...Elevations[2]}}>
     <Text style={{fontSize:15}}>
     Update
     </Text>
     </Button>
+    
      </SafeAreaView>
   );
 }
 
 const styles=StyleSheet.create({
     container:{
-      flex:5,
-      height:100,
+        paddingTop:"5px",
+paddingLeft:"10px",
+      height:500,
         backgroundColor:"lightgray",
-        flexDirection:"row",
-        justifyContent:"center",
-
     },
     img:{
         height:100,
         width:100,
+    },
+    txtInput:{
+fontSize:18,
+fontWeight:"bold",
+marginBottom:"10px"
     }
 })
